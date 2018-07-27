1531bfd065b6e44227abe839a0bd2e0e3d563004
Peter Pham <peterpham@Peters-MacBook.local>
2018-07-26 21:32:38 -0700

    reduce false trigger of amazon dash button with more strict filtering

--- amazon_dash/scan.py
+++ amazon_dash/scan.py
@@ -21,6 +21,6 @@ def scan_devices(fn, lfilter, iface=None):
     :return: loop
     """
     try:
-        sniff(prn=fn, store=0, filter="udp", lfilter=lfilter, iface=iface)
+        sniff(prn=fn, store=0, filter="(udp and (port 67 or 68))", lfilter=lfilter, iface=iface)
     except PermissionError:
         raise SocketPermissionError
